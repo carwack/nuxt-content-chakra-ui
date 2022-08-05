e32d0c069fde2465120638a7dffb6a7e8d1b165f
carwack <sybren.willemot@gmail.com>
2022-08-05 19:05:09 +0200

    🚀 Initial commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { CButton, CReset } from '@chakra-ui/vue-next'
+</script>
+
+<template>
+	<div>
+		<NuxtPage />
+	</div>
+</template>
---
+++ pages/[...slug].vue
@@ -0,0 +1,10 @@
+<script setup>
+import { CButton } from '@chakra-ui/vue-next'
+</script>
+
+<template>
+	<main>
+		<CButton>Hallo</CButton>
+		<ContentDoc />
+	</main>
+</template>
