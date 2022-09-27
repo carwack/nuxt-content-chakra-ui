07b637f133acc978cac9439f8f8e6b29d43fc2e4
Daniel Roe <daniel@roe.dev>
2022-09-27 21:05:21 +0100

    fix: don't inline emotion in the nitro dev server

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 export default defineNuxtConfig({
   modules: [
     '@nuxt/content',
+    function (options, nuxt) {
+      nuxt.hook('nitro:config', config => {
+        if (nuxt.options.dev) {
+          // Prevent inlining emotion (+ the crucial css cache!) in dev mode
+          config.externals.external ||= []
+          config.externals.external.push('@emotion/server')
+        }
+      })
+    }
   ],
 })
