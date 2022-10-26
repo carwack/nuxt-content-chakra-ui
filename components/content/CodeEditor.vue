223e2107efb5b3aa147ee39cebb3ee9f1fc46fd0
carwack <sybren.willemot@gmail.com>
2022-10-26 19:15:19 +0200

    feat: add code editor layout

--- components/content/CodeEditor.vue
+++ components/content/CodeEditor.vue
@@ -1,11 +1,12 @@
 <template>
-	<VueLive :code="code" :components="Chakra" />
+	<VueLive :code="code" :components="Chakra" :layout="CodeEditorLayout" />
 </template>
 
 <script setup lang="ts">
 import { defineProps } from 'vue'
 import 'vue-live/lib/vue-live.esm.css'
 import { VueLive } from 'vue-live'
+import CodeEditorLayout from './Editor/CodeEditorLayout.vue'
 
 import * as Chakra from '@chakra-ui/vue-next'
 
